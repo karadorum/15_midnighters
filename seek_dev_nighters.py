5cfc1b5500baa448962a7354c231977a8e85cfb0
karadorum <contextstudio78@gmail.com>
2018-12-18 20:24:28 +0300

    Added functions

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,45 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+
+def get_number_of_pages():
+    first_page = 1
+    url = 'https://devman.org/api/challenges/solution_attempts/?page={}'.format(first_page)
+    response = requests.get(url)
+    number_of_pages = response.json()['number_of_pages']
+    return number_of_pages
+    
+
+def load_attempts(pages):
+    url = 'https://devman.org/api/challenges/solution_attempts'
+    for page in range(1, pages):
+        request_params = {'page': page}  
+        response = requests.get(url, request_params)
+        json_api_page = response.json()['records']
+        for attempt in json_api_page:
+            yield attempt 
+        
+
+def get_midnighters(attempts):
+    midnighters = []
+    for attempt in attempts:
+        user_time = get_local_datetime(attempt)
+        start_hour = 0
+        end_hour = 6
+        if start_hour <= user_time.hour <= end_hour:
+            midnight_user = attempt.get('username')
+            if midnight_user not in midnighters:
+                midnighters.append(attempt.get('username'))
+    return midnighters
+
+
+def get_local_datetime(attempt):
+    user_timezone = pytz.timezone(attempt.get('timezone'))
+    user_time = datetime.fromtimestamp(attempt.get('timestamp'), tz= user_timezone)
+    return user_time
 
 if __name__ == '__main__':
-  pass
+    pages = get_number_of_pages()
+    print(get_midnighters(load_attempts(pages)))
